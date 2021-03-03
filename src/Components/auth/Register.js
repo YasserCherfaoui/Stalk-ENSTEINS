ca121bf65c83b7e48a88ed4f7a0966d3ee967223
yasser cherfaoui <y_cherfaoui@enst.dz>
2021-03-03 22:34:26 +0100

    adding register login functions to frontend

--- src/Components/auth/Register.js
+++ src/Components/auth/Register.js
@@ -1,8 +1,46 @@
 import React, { Component } from "react";
-
+import persons from "../../assets/User Profile_Monochromatic.svg";
+import google from "../../assets/google.svg";
+import apple from "../../assets/Path 1903.svg";
+import mail from "../../assets/mail.svg";
+import eye from "../../assets/show.svg";
+import person from "../../assets/user.svg";
 class Register extends Component {
+  handleLogin = () => {
+    window.location.reload();
+  };
   render() {
-    return <div className="register"></div>;
+    return (
+      <div className="register" hidden={this.props.hidden}>
+        <img src={persons} alt="persons" id="persons" />
+        <div id="jtt">JOIN THE TEAM</div>
+        <div id="ca">Create Account</div>
+        <div className="signin_methods r">
+          <button>
+            <img src={google} alt="google" />
+            Register with Google
+          </button>
+          <button>
+            <img src={apple} alt="apple" />
+            Register with Apple
+          </button>
+        </div>
+        <div id="ocue">Or Create Using Email</div>
+        <div className="signin_inputs re">
+          <input type="text" placeholder="YasserCherfaoui" />
+          <img src={person} alt="mail" />
+          <input type="email" placeholder="example@enst.dz" />
+          <img src={mail} alt="mail" />
+          <input type="password" placeholder="*******" />
+          <img src={eye} alt="eye" />
+          <button>Register</button>
+        </div>
+
+        <div className="dha reg">
+          Already a Team Member? <span onClick={this.handleLogin}>LOG IN</span>
+        </div>
+      </div>
+    );
   }
 }
 
--- src/Components/auth/SignIn.js
+++ src/Components/auth/SignIn.js
@@ -5,36 +5,49 @@ import apple from "../../assets/Path 1903.svg";
 import logoText from "../../assets/ENSTEINS.svg";
 import mail from "../../assets/mail.svg";
 import eye from "../../assets/show.svg";
+import Register from "./Register";
 class SignIn extends Component {
+  state = {
+    register: false,
+  };
+  handleRegister = () => {
+    this.setState({
+      register: true,
+    });
+  };
   render() {
     return (
-      <div className="signIn">
-        <img id="AppLogo" src={logo} alt="ENSTEINS" />
-        <img id="LogoText" src={logoText} alt="ENSTEINS" />
-        <div id="wb">WELCOME BACK</div>
-        <div id="ali">Account Log In</div>
-        <div className="signin_methods">
-          <button>
-            <img src={google} alt="google" />
-            Log In with Google
-          </button>
-          <button>
-            <img src={apple} alt="apple" />
-            Log In with Apple
-          </button>
+      <React.Fragment>
+        <Register hidden={this.state.register ? false : true} />
+        <div className="signIn" hidden={this.state.register ? true : false}>
+          <img id="AppLogo" src={logo} alt="ENSTEINS" />
+          <img id="LogoText" src={logoText} alt="ENSTEINS" />
+          <div id="wb">WELCOME BACK</div>
+          <div id="ali">Account Log In</div>
+          <div className="signin_methods">
+            <button>
+              <img src={google} alt="google" />
+              Log In with Google
+            </button>
+            <button>
+              <img src={apple} alt="apple" />
+              Log In with Apple
+            </button>
+          </div>
+          <div id="lwom">Or Log In Using Email</div>
+          <div className="signin_inputs">
+            <input type="email" placeholder="example@enst.dz" />
+            <img src={mail} alt="mail" />
+            <input type="password" placeholder="*******" />
+            <img src={eye} alt="eye" />
+            <button>Continue</button>
+          </div>
+          <div className="dha">
+            Don’t have an Account?{" "}
+            <span onClick={this.handleRegister}>REGISTER</span>
+          </div>
         </div>
-        <div id="lwom">Or Log In Using Email</div>
-        <div className="signin_inputs">
-          <input type="text" placeholder="example@enst.dz" />
-          <img src={mail} alt="mail" />
-          <input type="password" placeholder="*******" />
-          <img src={eye} alt="eye" />
-          <button>Continue</button>
-        </div>
-        <div id="dha">
-          Don’t have an Account? <span>REGISTER</span>
-        </div>
-      </div>
+      </React.Fragment>
     );
   }
 }
